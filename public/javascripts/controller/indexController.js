58cb36950056670c08227e77b8e542e2955d124a
Hakan <hakantuncer.software@gmail.com>
2024-07-03 18:04:33 +0300

    newUser Event-2

--- public/javascripts/controller/indexController.js
+++ public/javascripts/controller/indexController.js
@@ -1,5 +1,6 @@
 app.controller("indexController", ['$scope', 'indexFactory', ($scope, indexFactory) => {
 
+    $scope.messages = [ ];
     $scope.init = () => {
       const username = prompt('Enter username');
 
@@ -18,6 +19,16 @@ app.controller("indexController", ['$scope', 'indexFactory', ($scope, indexFacto
         indexFactory.connectSocket('http://localhost:3000', connectionOptions)
          .then((socket) => {
              socket.emit('newUser', { username });
+
+             socket.on('newUser', (data) => {
+                 const messageData = {
+                     type: 0, // info
+                     username: data.username
+                 };
+
+                 $scope.messages.push(messageData);
+                 $scope.$apply();
+             });
          }).catch((err) => {
           console.log(err);
          });
